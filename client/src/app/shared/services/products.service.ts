19a18b0df3a3564eb7b13c284f165583a306f583
xavimrg <javier.martin.mr@gmail.com>
2024-12-08 15:52:24 +0100

    metodos del crud en products-service

--- client/src/app/shared/services/products.service.ts
+++ client/src/app/shared/services/products.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { Product } from '../interfaces/product';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -22,4 +23,22 @@ getAllProducts(){
   subscribe(products=>this.products.set(products)) // me suscribo al signal procuts que le digo que es un set de products
 }
 
+addProduct(product: Product){ // le pasamos a la funcion un product con la estructura del Interfaz Product
+  return this.hhtp.post<Product>(this.Url, product) // devuelvo el serv. hhtp. con el metodo post <Product> (a esta url y le paso el product concreto)
 }
+
+getProductById(id: number){ // le pasamos el ID
+return this.hhtp.get<Product>(`${this.Url}/${id}`) // = pero añado el ID 
+
+}
+
+updateProduct(id: number, product: Product){ // en este caso usamos el ID para localizar y pasamos de nuevo el producto para actualizarlo
+return this.hhtp.put<Product>(`${this.Url}/${id}`, product) // usamos metodo put en lugar de post y añadimos el product de nuevo
+}
+
+deleteProduct(id: number): Observable<void>{ // localiazmos con el ID pasado y despues utilizamos el observable vacio
+  return this.hhtp.delete<void>(`${this.Url}/${id}`)  // metodo delete de http
+}
+
+}
+
