ea059c6c9c2ee46a855db9e749f97d4ea57efd0f
xavimrg <javier.martin.mr@gmail.com>
2024-12-11 15:34:08 +0100

    functional form - no product list reload

--- client/src/app/products/product-form/product-form.component.ts
+++ client/src/app/products/product-form/product-form.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ProductsService } from '../../shared/services/products.service';
+import { Product } from '../../shared/interfaces/product';
 
 @Component({
   selector: 'app-product-form',
@@ -12,10 +13,30 @@ import { ProductsService } from '../../shared/services/products.service';
 })
 export class ProductFormComponent {
 productForm! : FormGroup;
+@Output() productCreated = new EventEmitter<Product>();
+
 
 productService = inject(ProductsService)
 fb = inject(FormBuilder)
 
-
-onSubmit(){}
+constructor(){
+  this.productForm = this.fb.group({
+    name: ['', Validators.required],
+    imageUrl: ['', Validators.required],
+    price: [0, Validators.required],
+    description: ['',Validators.required],
+    stock: [0, Validators.required],
+    maximumStock:[0,Validators.required]
+  })
 }
+onSubmit() {
+  if (this.productForm.valid) {
+    this.productService.createProduct(this.productForm.value).subscribe((newProduct) => {
+      this.productCreated.emit(newProduct);
+      this.productForm.reset();
+    });
+  }
+}
+
+}
+
