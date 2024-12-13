c694fa8a8bb2a047bf860466d68fb122eea8de1c
xavimrg <javier.martin.mr@gmail.com>
2024-12-13 13:51:39 +0100

    first styles product list

--- client/src/app/products/product.component.ts
+++ client/src/app/products/product.component.ts
@@ -5,6 +5,7 @@ import { Product } from '../shared/interfaces/product';
 import { ProductFormComponent } from "./product-form/product-form.component";
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatButtonToggleModule} from '@angular/material/button-toggle';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-product',
@@ -16,13 +17,13 @@ export class ProductComponent implements OnInit {
 
   productService = inject(ProductsService);
   fb = inject(FormBuilder);
-
+  snackBar = inject(MatSnackBar) // pop up de Angular Material
   products = signal<Product[]>([])
 
   productForm!: FormGroup;
   isFormVisible: boolean = false;
-toggleGroup: any;
 
+toggleGroupValue = signal<string>('close') // gestion del estado del toggle - inicialmente
 
 
 ngOnInit(): void {
@@ -49,16 +50,35 @@ this.productService.getAllProducts().subscribe( // subscribe -> signal
   this.products.set(data) // setter de signals
 });
 }
+//METODO SIN MENSAJES Y CAMBIO DE ESTADO EN TOGGLE
+// onSubmit(product: Product): void{
+// this.productService.createProduct(product).subscribe((newProduct) => { // crea el nuevo producto y lo manda pero se suscribe a los signals
+//   this.products.set([...this.products(), newProduct]); // products() es el get de signals - no un metodo
+// });}
 
-onSubmit(product: Product): void{
-this.productService.createProduct(product).subscribe((newProduct) => { // crea el nuevo producto y lo manda pero se suscribe a los signals
-  this.products.set([...this.products(), newProduct]); // products() es el get de signals - no un metodo
-});}
-
-
-
-
+onSubmit(product: Product): void {
+  this.productService.createProduct(product).subscribe({ // AÑADIMOS EL NEXT para manejar el error y dividir las dos acciones 
+    next: (newProduct) => {
+      this.products.set([...this.products(), newProduct]); // actualiza la lista al añadir el nuevo producto
+      this.showNotification('Product successfully added.', 'success');
+      this.toggleGroupValue.set('close'); // setea el estado cerrado o product list
+    },
+    error: () => {
+      this.showNotification('Fail to add the new product.', 'error');
+    }
+  });
 }
+
+private showNotification(message: string, type: 'success' | 'error'): void {
+  this.snackBar.open(message, 'Close', {
+    duration: 3000,
+    panelClass: type === 'success' ? 'snackbar-success' : 'snackbar-error',
+  });
+}
+}
+
+
+
 
 
 
