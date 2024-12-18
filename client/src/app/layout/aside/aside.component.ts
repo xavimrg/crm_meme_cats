dbe3e809a219b9d91ce4ce9f40885a7b496a1c7e
xavimrg <javier.martin.mr@gmail.com>
2024-12-18 13:50:36 +0100

    routing - missing details

--- client/src/app/layout/aside/aside.component.ts
+++ client/src/app/layout/aside/aside.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-aside',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './aside.component.html',
   styleUrl: './aside.component.scss'
 })
